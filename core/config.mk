0fcf05f38f1f5eafe38ed707cd074f522e41f8d3
Luca Stefani <luca020400@lineageos.org>
2017-08-17 22:20:29 +0200

    core: Include QCOM/MTK targets
    [Potato: Kshitij Gupta]
    - Make inclusions more generic
      (potato -> *)
    
    Change-Id: I7e33db775e80f042ef71a43c67f32abd4b6038e7
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -1246,6 +1246,12 @@ $(eval include device/spark/sepolicy/common/sepolicy.mk)
 
 # Include any vendor specific apicheck.mk file
 -include $(TOPDIR)vendor/*/build/core/apicheck.mk
+
+# Rules for QCOM targets
+-include $(TOPDIR)vendor/*/build/core/qcom_target.mk
+
+# Rules for MTK targets
+-include $(TOPDIR)vendor/*/build/core/mtk_target.mk
 endif
 
 -include external/linux-kselftest/android/kselftest_test_list.mk
