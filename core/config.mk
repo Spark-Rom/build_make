da76cc189f048110880a670834f1d6bfd2250748
Kshitij Gupta <kshitijgm@gmail.com>
2018-10-10 08:20:13 +0000

    config: Don't setup qcom_target here
    - This is being done in vendor/potato. Doing it here sets path twice in
      respective variables. This causes guard makefiles to fail checks and
      not properly include HALs.
    
    Change-Id: Ie73566025adc459875dc382f65049e0138dc3794
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -1255,9 +1255,6 @@ $(eval include device/spark/sepolicy/common/sepolicy.mk)
 # Include any vendor specific apicheck.mk file
 -include $(TOPDIR)vendor/*/build/core/apicheck.mk
 
-# Rules for QCOM targets
--include $(TOPDIR)vendor/*/build/core/qcom_target.mk
-
 # Rules for MTK targets
 -include $(TOPDIR)vendor/*/build/core/mtk_target.mk
 endif
