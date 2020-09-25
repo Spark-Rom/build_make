66530331f34f77667973a294e5f4a1f653c4d337
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-09-25 11:21:46 +0300

    build: Bring back rom info on flashing
    [ender1324]:
    - add is_gapps function
    - add fluid version
    
    Change-Id: Ieb472ee32034018f4ce6daf54747cee25535f87d
    Signed-off-by: spkal01 <kalligeross@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -357,8 +357,8 @@ include $(BUILD_SYSTEM)/envsetup.mk
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 -include vendor/extra/BoardConfigExtra.mk
-ifneq ($(LINEAGE_BUILD),)
-include vendor/lineage/config/BoardConfigLineage.mk
+ifneq ($(SPARK_BUILD),)
+include vendor/spark/config/BoardConfigSpark.mk
 endif
 
 # The build system exposes several variables for where to find the kernel
@@ -1264,11 +1264,11 @@ dont_bother_goals := out product-graph
 # consistency with those defined in BoardConfig.mk files.
 include $(BUILD_SYSTEM)/android_soong_config_vars.mk
 
-ifneq ($(LINEAGE_BUILD),)
-ifneq ($(wildcard device/lineage/sepolicy/common/sepolicy.mk),)
+ifneq ($(SPARK_BUILD),)
+ifneq ($(wildcard device/spark/sepolicy/common/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include device/lineage/sepolicy/common/sepolicy.mk)
+$(eval include device/spark/sepolicy/common/sepolicy.mk)
 endif
 endif
 
