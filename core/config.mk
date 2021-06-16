a8d12d41338325239c27ff095d1c9dbf935168b7
spkal01 <kalligeross@gmail.com>
2021-06-16 13:36:47 +0530

    build_make: Properly include spark sepolicy

--- core/config.mk
+++ core/config.mk
@@ -1238,14 +1238,13 @@ dont_bother_goals := out \
     vbmetaimage-nodeps \
     product-graph dump-products
 
+ifneq ($(wildcard device/spark/sepolicy/common/sepolicy.mk),)
+$(eval include device/spark/sepolicy/common/sepolicy.mk)
+endif
 ifeq ($(CALLED_FROM_SETUP),true)
 include $(BUILD_SYSTEM)/ninja_config.mk
 include $(BUILD_SYSTEM)/soong_config.mk
 endif
-
-## We need to be sure the global selinux policies are included
-## last, to avoid accidental resetting by device configs
-$(eval include device/spark/sepolicy/common/sepolicy.mk)
 
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk
